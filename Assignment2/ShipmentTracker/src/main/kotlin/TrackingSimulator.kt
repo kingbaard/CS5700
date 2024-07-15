569904ab88755127b5f114ebf1adde859463faa1
kingbaard <bradley.rod.jensen@gmail.com>
2024-07-14 19:54:49 -0600

    fleshing out TrackingSimulator class

--- Assignment2/ShipmentTracker/src/main/kotlin/TrackingSimulator.kt
+++ Assignment2/ShipmentTracker/src/main/kotlin/TrackingSimulator.kt
@@ -1,18 +1,50 @@
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
+import java.io.File
 
-class TrackingSimulator {
-    val shipments = remember { mutableStateListOf<Shipment>() }
+object TrackingSimulator {
+    val shipments =  mutableStateListOf<Shipment>()
+    val validShipmentIds : Set<String> = getValidIds()
+    val file = File("test.txt")
+    var time : Int? = getStartingTime()
 
-//    findShipment(id: String): Shipment? {
-//        return null
-//    }
+    fun findShipment(id: String): Shipment? {
+        if (validShipmentIds.contains(id)) {
 
-//    addShipment(shiptment: Shipment) {
-//
-//    }
-//
-//    runSimulation() {
-//
-//    }
+        }
+        else {
+            return null
+        }
+    }
+
+    fun addShipment(shipment: Shipment) {
+        shipments.add(shipment)
+    }
+
+    fun runSimulation() {
+        // If there are any updates in this second
+
+    }
+
+    fun readSecond() {}
+
+    fun getStartingTime() : Int? {
+        file.useLines { lines ->
+            val firstLine = lines.firstOrNull()
+            if (firstLine != null) {
+                return firstLine.split(',')[2].toInt()
+            }
+            return null
+        }
+    }
+
+    fun getValidIds() : Set<String> {
+        val uniqueIds = mutableSetOf<String>()
+        file.forEachLine { line ->
+            val columns = line.split(',')
+            if (columns[3].toInt() > time!!) return@forEachLine
+            uniqueIds.add(columns[2])
+        }
+        return uniqueIds
+    }
 }
